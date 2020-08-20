aa35725eb45fa50bcca87ee6391b150f18906fa0
elanurisik <elanurisik1352@gmail.com>
2020-08-20 17:36:25 +0300

    ela

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React plz again
+          !Learn React plz again
         </a>
       </header>
     </div>
