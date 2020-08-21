4836a2dbd932aea7a8a94068160e0c357fc9a632
busraUnver <busra.unver@ug.bilkent.edu.tr>
2020-08-21 12:45:23 +0300

    Updates

---
+++ precache-manifest.88984ab7955c27f17b20890dcd5f13e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d15ce16f56a7d3bdd129727a3f71b8",
+    "url": "/webtutorial/index.html"
+  },
+  {
+    "revision": "8806d827823049535e1f",
+    "url": "/webtutorial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "669f89ec64d61f651819",
+    "url": "/webtutorial/static/js/2.488a2f15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webtutorial/static/js/2.488a2f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8806d827823049535e1f",
+    "url": "/webtutorial/static/js/main.7fc90c4f.chunk.js"
+  },
+  {
+    "revision": "f61ff6a3d39b5781dd25",
+    "url": "/webtutorial/static/js/runtime-main.75e66078.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webtutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
