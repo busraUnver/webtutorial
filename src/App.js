58d67dbe3107bcfdbca692a012ad33c347051c80
busraUnver <busra.unver@ug.bilkent.edu.tr>
2021-05-06 03:33:39 +0300

    pls

--- src/App.js
+++ src/App.js
@@ -3,17 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={handleClick}
         >
-          !Learn Sena React plz again.....
+          Ela
+        </a>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={handleClick}
+        >
+          Kutay
+        </a>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={handleClick}
+        >
+          Mustafa
+        </a>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={handleClick}
+        >
+          Güneş
         </a>
       </header>
     </div>
